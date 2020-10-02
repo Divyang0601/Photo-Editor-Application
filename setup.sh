9d68698d5830bf2fb9b8d93d1c72a1095f9f9490
Divyang Mishra <66544115+Divyang0601@users.noreply.github.com>
2020-10-02 21:14:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo “
+[server]
+headless = true
+enableCORS=false
+port = $PORT
+“ > ~/.streamlit/config.toml
